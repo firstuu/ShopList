1a17933c71172d44b8363c5249b8ea1979a2b5fe
Tomasz First <tom222211@gmail.com>
2024-11-27 00:00:21 +0100

    project configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,16 +1,16 @@
 import type { Config } from "tailwindcss";
 
 export default {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: "#333333",
+        secondary: "#E5E5E5",
+        background: "#F5F5F5",
+        "accent-green": "#52B788",
+        "accent-blue": "#3B82F6",
+        "accent-red": "E63946",
       },
     },
   },
